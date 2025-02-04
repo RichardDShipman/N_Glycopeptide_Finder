37e11c051edd679fe5a40d21cef6d3c2e8485940
Richard D Shipman <djshipman2011@gmail.com>
2025-02-04 03:54:36 +0100

    batch file update with glycosylation type

---
+++ batch_glycopeptide_sequence_finder.sh
@@ -0,0 +1,1 @@
+ls test_proteomes/*.fasta | xargs -I {} -P 4 python glycopeptide_sequence_finder_cmd.py -i "{}" -p trypsin -g N -c 0 -v
--- batch_n_glycopeptide_sequence_finder.sh
+++
@@ -1,1 +0,0 @@
-ls test_proteomes/*.fasta | xargs -I {} -P 4 python n_glycopeptide_sequence_finder_cmd.py -i "{}" -p trypsin -g N -c 0 -v
